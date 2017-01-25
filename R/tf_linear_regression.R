8aa7024fd76a84bd6fd0553ff8ac96acc6ad0e09
Kevin Ushey <kevinushey@gmail.com>
2017-01-24 16:16:26 -0800

    tidy up + remove unused functions

--- R/tf_linear_regression.R
+++ R/tf_linear_regression.R
@@ -10,8 +10,10 @@
 #' @examples
 #' recipe <- tf_simple_recipe(mpg ~ drat, data = mtcars)
 #' tf_linear_regression(recipe = recipe)
-tf_linear_regression <- function(recipe, tf.options = tf_options(), ...) {
-
+tf_linear_regression <- function(recipe,
+                                 tf.options = tf_options(),
+                                 ...)
+{
   # extract feature columns
   feature_columns <- recipe$feature.columns
   if (is.function(feature_columns))
@@ -38,29 +40,33 @@ tf_linear_regression <- function(recipe, tf.options = tf_options(), ...) {
 
 }
 
-tf_linear_classification <- function(recipe, tf.options = tf_options(), ...) {
-
+# TODO
+tf_linear_classification <- function(recipe,
+                                     tf.options = tf_options(),
+                                     ...)
+{
   # extract feature columns
   feature_columns <- recipe$feature.columns
   if (is.function(feature_columns))
     feature_columns <- feature_columns()
 
   # construct estimator accepting those columns
-  lr <- learn$LinearClassifier(
+  lc <- learn$LinearClassifier(
     feature_columns = feature_columns,
+    n_classes = recipe$n.classes %||% 2L,
     model_dir = tf.options$model.dir,
     optimizer = tf.options$optimizer
   )
 
   # fit the model
-  lr$fit(
+  lc$fit(
     input_fn = recipe$input.fn,
     steps = tf.options$steps
   )
 
   tf_model(
-    "linear_regression",
-    estimator = lr,
+    "linear_classification",
+    estimator = lc,
     recipe = recipe
   )
 
