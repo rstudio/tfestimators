211f7bcdfcf8b65bdc1c666da42265c874cd6567
terrytangyuan <terrytangyuan@gmail.com>
2017-02-01 22:55:52 -0600

    Moved tf_models test to separate file

--- tests/testthat/test-linear-dnn_combined-estimators.R
+++ tests/testthat/test-linear-dnn_combined-estimators.R
@@ -29,18 +29,3 @@ test_that("linear_dnn_combined_classification() runs successfully", {
   expect_equal(predictions1, predictions2)
   expect_equal(coefs1, coefs2)
 })
-
-test_that("predict() can accept new input_fn() from recipe", {
-  recipe <- simple_linear_dnn_combined_recipe(mtcars, response = "mpg", linear.features = c("cyl"), dnn.features = c("drat"))
-  reg <- linear_dnn_combined_regression(recipe = recipe, dnn_hidden_units = c(1L, 1L), dnn_optimizer = "Adagrad")
-  predictions <- predict(reg, input_fn = recipe$input.fn)
-  expect_equal(length(predictions), 32)
-})
-
-test_that("predict() can produce probabilities of predictions", {
-  recipe <- simple_linear_dnn_combined_recipe(mtcars, response = "vs", linear.features = c("cyl"), dnn.features = c("drat"))
-  reg <- linear_dnn_combined_classification(recipe = recipe, dnn_hidden_units = c(1L, 1L), dnn_optimizer = "Adagrad")
-  prediction_probs <- predict(reg, input_fn = recipe$input.fn, type = "prob")
-  expect_true(prediction_probs <= 1 && prediction_probs >=0)
-  expect_equal(length(prediction_probs), 64)
-})
