6c193e5dc0f82881a06b287903b0f2f09cf65baf
terrytangyuan <terrytangyuan@gmail.com>
2017-02-04 13:15:04 -0600

    Remove one test that's not true

--- tests/testthat/test-linear-dnn_combined-estimators.R
+++ tests/testthat/test-linear-dnn_combined-estimators.R
@@ -10,21 +10,10 @@ test_that("linear_dnn_combined_regression() runs successfully", {
 })
 
 test_that("linear_dnn_combined_classification() runs successfully", {
-  # Without casting "vs" to factor
-  recipe <- simple_linear_dnn_combined_recipe(mtcars, response = "vs", linear.features = c("cyl"), dnn.features = c("drat"))
-  reg1 <- linear_dnn_combined_classification(recipe = recipe, dnn_hidden_units = c(3L, 3L), dnn_optimizer = "Adagrad")
-  coefs1 <- coef(reg1)
-  expect_warning(predictions1 <- predict(reg1))
-  expect_equal(length(predictions1), 32)
 
-  # Casting "vs" to factor
   mtcars$vs <- as.factor(mtcars$vs)
   recipe <- simple_linear_dnn_combined_recipe(mtcars, response = "vs", linear.features = c("cyl"), dnn.features = c("drat"))
-  reg2 <- linear_dnn_combined_classification(recipe = recipe, dnn_hidden_units = c(3L, 3L), dnn_optimizer = "Adagrad")
-  coefs2 <- coef(reg2)
-  expect_warning(predictions2 <- predict(reg2))
-
-  # Should be the same model
-  expect_equal(predictions1, predictions2)
-  expect_equal(coefs1, coefs2)
+  clf <- linear_dnn_combined_classification(recipe = recipe, dnn_hidden_units = c(3L, 3L), dnn_optimizer = "Adagrad")
+  coefs <- coef(clf)
+  expect_warning(predictions <- predict(clf))
 })
