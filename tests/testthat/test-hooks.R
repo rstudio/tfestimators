b7c2f74ebb89a93c88ec2793b853c227f74cb7ed
terrytangyuan <terrytangyuan@gmail.com>
2017-02-14 22:42:58 -0600

    Switched to use py_capture_output

--- tests/testthat/test-hooks.R
+++ tests/testthat/test-hooks.R
@@ -14,7 +14,7 @@ test_that("Hooks works with linear dnn combined estimators", {
   tf$logging$set_verbosity(tf$logging$INFO)
   logging_hook <- tf$python$training$basic_session_run_hooks$LoggingTensorHook
   
-  output <- reticulate::py_capture_stdout(
+  output <- reticulate:::py_capture_output(
     linear_dnn_combined_regression(
       recipe = recipe,
       dnn_hidden_units = c(1L, 1L),
@@ -27,5 +27,3 @@ test_that("Hooks works with linear dnn combined estimators", {
   )
   tf$logging$set_verbosity(old_verbose_level)
 })
-
-
