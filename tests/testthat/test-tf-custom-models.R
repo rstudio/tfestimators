da761d36f7019f07a6efd6dabb0864fc94497b4a
terrytangyuan <terrytangyuan@gmail.com>
2017-04-16 19:42:14 -0500

    Fixed tests for custom model

--- tests/testthat/test-tf-custom-models.R
+++ tests/testthat/test-tf-custom-models.R
@@ -1,21 +1,28 @@
 context("Testing tf_custom_models methods")
 
-test_that("custom model works on iris data", {
+source("utils.R")
 
-  constructed_input_fn <- input_fn(
+test_that("custom model works on iris data", {
+  
+  input_fn_batch_size <- 10L
+  features <- c(
+    "Sepal.Length",
+    "Sepal.Width",
+    "Petal.Length",
+    "Petal.Width")
+  num_features <- length(features)
+
+  constructed_input_fn <- numpy_input_fn(
     x = iris,
     response = "Species",
-    features = c(
-      "Sepal.Length",
-      "Sepal.Width",
-      "Petal.Length",
-      "Petal.Width"),
-    features_as_named_list = FALSE
+    features = features,
+    features_as_named_list = FALSE,
+    batch_size = input_fn_batch_size
   )
-
-  custom_model_fn <- function(features, labels, mode, params, config) {
-    labels <- tf$one_hot(labels, 3L)
-
+  
+  simple_custom_model_fn <- function(features, labels, mode, params, config) {
+    names(features) <- NULL
+    features <- tf$reshape(tf$stack(features), c(input_fn_batch_size, num_features))
     # Create three fully connected layers respectively of size 10, 20, and 10 with
     # each layer having a dropout probability of 0.1.
     logits <- features %>%
@@ -24,27 +31,33 @@ test_that("custom model works on iris data", {
         normalizer_fn = tf$contrib$layers$dropout,
         normalizer_params = list(keep_prob = 0.9)) %>%
       tf$contrib$layers$fully_connected(3L, activation_fn = NULL) # Compute logits (1 per class) and compute loss.
-
-    loss <- tf$losses$softmax_cross_entropy(labels, logits)
+    
     predictions <- list(
       class = tf$argmax(logits, 1L),
       prob = tf$nn$softmax(logits))
-
+    
+    if(mode == "infer"){
+      return(estimator_spec(predictions = predictions, mode = mode, loss = NULL, train_op = NULL))
+    }
+    
+    labels <- tf$one_hot(labels, 3L)
+    loss <- tf$losses$softmax_cross_entropy(labels, logits)
+    
     # Create a tensor for training op.
     train_op <- tf$contrib$layers$optimize_loss(
       loss,
       tf$contrib$framework$get_global_step(),
       optimizer = 'Adagrad',
       learning_rate = 0.1)
-
+    
     return(estimator_spec(predictions, loss, train_op, mode))
   }
-
+  
   classifier <- estimator(
-      model_fn = custom_model_fn) %>%
-    fit(input_fn = constructed_input_fn)
-  ## TODO: Fix the issue on Python end
-  # predictions <- predict(classifier, input_fn = constructed_input_fn, type = "prob")
+    model_fn = simple_custom_model_fn) %>%
+    fit(input_fn = constructed_input_fn, steps = 2L)
+  predictions <- predict(classifier, input_fn = constructed_input_fn)
+  
   # expect_equal(length(predictions), 150 * length(unique(iris$Species)))
   # expect_lte(max(predictions), 1)
   # expect_gte(min(predictions), 0)
@@ -52,46 +65,3 @@ test_that("custom model works on iris data", {
   # expect_gt(length(coef(classifier)), 1) # TODO: Add get_variable_names attr on Python end
   expect_equal(names(evaluate(classifier, constructed_input_fn)), c("loss", "global_step"))
 })
-
-test_that("custom model works on mtcars data", {
-
-  mtcars$vs <- as.factor(mtcars$vs)
-  constructed_input_fn <- input_fn(
-    mtcars,
-    response = "vs",
-    features = c("drat", "cyl"),
-    features_as_named_list = FALSE
-  )
-
-  custom_model_fn <- function(features, labels, mode, params, config) {
-    labels <- tf$one_hot(labels, 2L)
-
-    # Create three fully connected layers respectively of size 10, 20, and 10 with
-    # each layer having a dropout probability of 0.1.
-    logits <- features %>%
-      tf$contrib$layers$stack(
-        tf$contrib$layers$fully_connected, c(10L, 20L, 10L),
-        normalizer_fn = tf$contrib$layers$dropout,
-        normalizer_params = list(keep_prob = 0.9)) %>%
-      tf$contrib$layers$fully_connected(2L, activation_fn = NULL) # Compute logits (1 per class) and compute loss.
-
-    loss <- tf$losses$softmax_cross_entropy(labels, logits)
-    predictions <- list(
-      class = tf$argmax(logits, 1L),
-      prob = tf$nn$softmax(logits))
-
-    # Create a tensor for training op.
-    train_op <- tf$contrib$layers$optimize_loss(
-      loss,
-      tf$contrib$framework$get_global_step(),
-      optimizer = 'Adagrad',
-      learning_rate = 0.1)
-
-    return(estimator_spec(predictions, loss, train_op, mode))
-  }
-  
-  classifier <- estimator(
-    model_fn = custom_model_fn) %>%
-    fit(input_fn = constructed_input_fn)
-})
-
--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -31,35 +31,3 @@ simple_dummy_model_fn <- function(features, labels, mode, params, config) {
   return(estimator_spec(predictions, loss, train_op, mode))
 }
 
-simple_custom_model_fn <- function(features, labels, mode, params, config) {
-  names(features) <- NULL
-  features <- tf$stack(unlist(features))
-  # Create three fully connected layers respectively of size 10, 20, and 10 with
-  # each layer having a dropout probability of 0.1.
-  logits <- features %>%
-    tf$contrib$layers$stack(
-      tf$contrib$layers$fully_connected, c(10L, 20L, 10L),
-      normalizer_fn = tf$contrib$layers$dropout,
-      normalizer_params = list(keep_prob = 0.9)) %>%
-    tf$contrib$layers$fully_connected(3L, activation_fn = NULL) # Compute logits (1 per class) and compute loss.
-
-  predictions <- list(
-    class = tf$argmax(logits, 1L),
-    prob = tf$nn$softmax(logits))
-
-  if(mode == "infer"){
-    return(estimator_spec(predictions = predictions, mode = mode, loss = NULL, train_op = NULL))
-  }
-
-  labels <- tf$one_hot(labels, 3L)
-  loss <- tf$losses$softmax_cross_entropy(labels, logits)
-
-  # Create a tensor for training op.
-  train_op <- tf$contrib$layers$optimize_loss(
-    loss,
-    tf$contrib$framework$get_global_step(),
-    optimizer = 'Adagrad',
-    learning_rate = 0.1)
-
-  return(estimator_spec(predictions, loss, train_op, mode))
-}
