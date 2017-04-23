02162b5b2385da9829dcdb9f1499d3fe5bf78ecd
terrytangyuan <terrytangyuan@gmail.com>
2017-04-23 14:50:46 -0400

    Added dynamic_rnn_estimator

--- R/rnn_estimators.R
+++ R/rnn_estimators.R
@@ -49,3 +49,48 @@ state_saving_rnn_estimator <- function(
     estimator = rnn_estimator
   )
 }
+
+#' @export
+dynamic_rnn_estimator <- function(
+  problem_type,
+  prediction_type,
+  sequence_feature_columns,
+  context_feature_columns = NULL,
+  num_classes = NULL,
+  num_units = NULL,
+  cell_type = 'basic_rnn',
+  optimizer = 'SGD',
+  learning_rate = 0.1,
+  predict_probabilities = F,
+  momentum = NULL,
+  gradient_clipping_norm = 5.0,
+  dropout_keep_probabilities = NULL,
+  feature_engineering_fn = NULL,
+  run_options = NULL)
+{
+  run_options <- run_options %||% run_options()
+  rnn_estimator <- contrib_estimators_lib$dynamic_rnn_estimator$DynamicRnnEstimator(
+    problem_type = problem_type,
+    prediction_type = prediction_type,
+    sequence_feature_columns = sequence_feature_columns,
+    context_feature_columns = context_feature_columns,
+    num_classes = as_nullable_integer(num_classes),
+    num_units = as_nullable_integer(num_units),
+    cell_type = cell_type,
+    optimizer = optimizer,
+    learning_rate = learning_rate,
+    predict_probabilities = predict_probabilities,
+    momentum = momentum,
+    gradient_clipping_norm = gradient_clipping_norm,
+    dropout_keep_probabilities = dropout_keep_probabilities,
+    feature_engineering_fn = feature_engineering_fn,
+    model_dir = run_options$model_dir,
+    config = run_options$run_config
+  )
+  
+  tf_model(
+    c("dynamic_rnn", "estimator"),
+    estimator = rnn_estimator
+  )
+}
+
--- tests/testthat/test-rnn-estimators.R
+++ tests/testthat/test-rnn-estimators.R
@@ -2,14 +2,18 @@ context("Testing rnn estimators")
 
 source("utils.R")
 
-test_that("state_saving_rnn_estimator works on sin sequence data", {
-  np <- import("numpy", convert = FALSE)
+np <- import("numpy", convert = FALSE)
+random_ops <- tf$python$ops$random_ops
+math_ops <- tf$python$ops$math_ops
+array_ops <- tf$python$ops$array_ops
+functional_ops <- tf$python$ops$functional_ops
 
+test_that("state_saving_rnn_estimator works on sine sequence data", {
   batch_size <- 8L
   num_unroll <- 5L
   sequence_length <- 64L
-  train_steps <- 250L
-  eval_steps <- 20L
+  train_steps <- 2L
+  eval_steps <- 2L
   num_rnn_layers <- 1L
   num_units <- rep(4L, num_rnn_layers)
   learning_rate <- 0.3
@@ -21,7 +25,7 @@ test_that("state_saving_rnn_estimator works on sin sequence data", {
   )
   
   rnn_sequence_estimator <- state_saving_rnn_estimator(
-    contrib_estimators_lib$constants$ProblemType$LINEAR_REGRESSION,
+    problem_type = contrib_estimators_lib$constants$ProblemType$LINEAR_REGRESSION,
     num_units = num_units,
     cell_type = 'lstm',
     num_unroll = num_unroll,
@@ -36,19 +40,19 @@ test_that("state_saving_rnn_estimator works on sin sequence data", {
   get_sin_input_fn <- function(sequence_length, increment, seed = NULL) {
     list(
       input_fn = function() {
-        start <- tf$python$ops$random_ops$random_uniform(
-          tuple(), minval = 0, maxval = pi * 2.0,
+        start <- random_ops$random_uniform(
+          list(), minval = 0, maxval = pi * 2.0,
           dtype = tf$python$framework$dtypes$float32, seed = seed)
-        sin_curves <- tf$python$ops$math_ops$sin(
-          tf$python$ops$math_ops$linspace(
+        sin_curves <- math_ops$sin(
+          math_ops$linspace(
             start, (sequence_length - 1) * increment,
             as.integer(sequence_length + 1)))
-        inputs <- tf$python$ops$array_ops$slice(sin_curves,
-                                                np$array(list(0), dtype = np$int64),
-                                                np$array(list(sequence_length), dtype = np$int64))
-        labels <- tf$python$ops$array_ops$slice(sin_curves,
-                                                np$array(list(1), dtype = np$int64),
-                                                np$array(list(sequence_length), dtype = np$int64))
+        inputs <- array_ops$slice(sin_curves,
+                                  np$array(list(0), dtype = np$int64),
+                                  np$array(list(sequence_length), dtype = np$int64))
+        labels <- array_ops$slice(sin_curves,
+                                  np$array(list(1), dtype = np$int64),
+                                  np$array(list(sequence_length), dtype = np$int64))
         return(tuple(list(inputs = inputs), labels))
       },
       features_as_named_list = TRUE)
@@ -61,3 +65,69 @@ test_that("state_saving_rnn_estimator works on sin sequence data", {
 
   # TODO: Add predict for tf_model with type estimator
 })
+
+
+test_that("dynamic_rnn_estimator works on sine sequence data", {
+  
+  batch_size <- 8L
+  sequence_length <- 64L
+  train_steps <- 2L
+  eval_steps <- 2L
+  num_rnn_layers <- 1L
+  num_units <- rep(4L, num_rnn_layers)
+  learning_rate <- 0.3
+  loss_threshold <- 0.035
+  dropout_keep_probabilities <- rep(0.9, num_rnn_layers + 1)
+  
+  seq_columns <- list(
+    tf$contrib$layers$feature_column$real_valued_column('inputs', dimension = num_units[1])
+  )
+  
+  rnn_sequence_estimator <- dynamic_rnn_estimator(
+    problem_type = contrib_estimators_lib$constants$ProblemType$LINEAR_REGRESSION,
+    prediction = contrib_estimators_lib$rnn_common$PredictionType$MULTIPLE_VALUE,
+    num_units = num_units,
+    cell_type = 'lstm',
+    sequence_feature_columns = seq_columns,
+    learning_rate = learning_rate,
+    dropout_keep_probabilities = dropout_keep_probabilities
+  )
+  
+  get_sin_input_fn <- function(batch_size, sequence_length, increment, seed = NULL) {
+    list(
+      input_fn = function() {
+        starts <- random_ops$random_uniform(
+          list(batch_size), minval = 0, maxval = pi * 2.0,
+          dtype = tf$python$framework$dtypes$float32, seed = seed)
+        sin_curves <- functional_ops$map_fn(
+          function(x){
+            math_ops$sin(
+              math_ops$linspace(
+                array_ops$reshape(x[1], list()),
+                (sequence_length - 1) * increment,
+                as.integer(sequence_length + 1)))
+          },
+          tuple(starts),
+          dtype = tf$python$framework$dtypes$float32
+        )
+        inputs <- array_ops$expand_dims(
+          array_ops$slice(
+            sin_curves,
+            np$array(list(0, 0), dtype = np$int64),
+            np$array(list(batch_size, sequence_length), dtype = np$int64)),
+            2L
+          )
+        labels <- array_ops$slice(sin_curves,
+                                  np$array(list(0, 1), dtype = np$int64),
+                                  np$array(list(batch_size, sequence_length), dtype = np$int64))
+        return(tuple(list(inputs = inputs), labels))
+      },
+      features_as_named_list = TRUE)
+  }
+  
+  train_input_fn <- get_sin_input_fn(batch_size, sequence_length, pi / 32, seed = 1234)
+  eval_input_fn <- get_sin_input_fn(batch_size, sequence_length, pi / 32, seed = 4321)
+  
+  fit(rnn_sequence_estimator, input_fn = train_input_fn, steps = train_steps)
+})
+
