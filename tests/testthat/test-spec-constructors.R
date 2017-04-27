b63f2ad8a444898b08667e1c92aba2e9248adc45
terrytangyuan <terrytangyuan@gmail.com>
2017-04-26 22:17:14 -0500

    Added test for input_fn.list

--- tests/testthat/test-spec-constructors.R
+++ tests/testthat/test-spec-constructors.R
@@ -31,6 +31,22 @@ test_that("input_fn can be constructed correctly from data.frame objects", {
   input_fn1 <- input_fn(mtcars, response = "mpg", features = features)$input_fn
   expect_equal(length(input_fn1()), 2)
   expect_equal(length(input_fn1()[[1]]), length(features))
-  
+})
+
+test_that("input_fn can be constructed correctly from list objects", {
+  fake_sequence_input_fn <-
+    input_fn(
+      x = list(
+        features = list(
+          list(list(1), list(2), list(3)),
+          list(list(4), list(5), list(6))),
+        response = list(
+          list(1, 2, 3), list(4, 5, 6))),
+      features = c("features"),
+      response = "response")$input_fn()
+
+  expect_equal(length(fake_sequence_input_fn), 2)
+  expect_true(inherits(fake_sequence_input_fn[[1]][[1]], "tensorflow.python.framework.ops.Tensor"))
+  expect_true(inherits(fake_sequence_input_fn[[2]], "tensorflow.python.framework.ops.Tensor"))
 })
 
