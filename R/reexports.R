3484fb058891bb42566500a6b1c0bc627db74c81
terrytangyuan <terrytangyuan@gmail.com>
2017-05-14 17:57:02 -0400

    reexports.R borrowed from keras project

---
+++ R/reexports.R
@@ -0,0 +1,26 @@
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @import magrittr
+#' @usage lhs \%>\% rhs
+NULL
+
+#' @export
+reticulate::use_python
+
+#' @export
+reticulate::use_virtualenv
+
+#' @export
+reticulate::use_condaenv
+
+#' @export
+tensorflow::install_tensorflow
+
+#' @export
+tensorflow::tf_config
