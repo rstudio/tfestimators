77cd8483e4f7ce8e643194e3a77581c1ad12e9e2
terrytangyuan <terrytangyuan@gmail.com>
2017-05-18 21:21:47 -0400

    wip svm estimator

--- R/svm_estimators.R
+++ R/svm_estimators.R
@@ -5,10 +5,9 @@
 svm_classifier <- function(feature_columns,
                            example_id_column,
                            weight_column_name = NULL,
-                           run_options = NULL,
-                           ...)
+                           model_dir = NULL,
+                           config = NULL)
 {
-  run_options <- run_options %||% run_options()
 
   # extract feature columns
   feature_columns <- resolve_fn(feature_columns)
@@ -17,9 +16,8 @@ svm_classifier <- function(feature_columns,
     feature_columns = feature_columns,
     example_id_column = example_id_column,
     weight_column_name = weight_column_name,
-    model_dir = run_options$model_dir,
-    config = run_options$run_config,
-    ...
+    model_dir = model_dir,
+    config = config
   )
 
   tf_model(
--- tests/testthat/test-svm-estimators.R
+++ tests/testthat/test-svm-estimators.R
@@ -8,12 +8,24 @@ test_that("svm_classification() runs successfully on mtcars data", {
     mtcars,
     response = "vs",
     features = c("drat", "cyl", "id_column"))
+  
+  dt <- data.frame(example_id = c("1", "2", "3"),
+                   price = c(0.6, 0.8, 0.3),
+                   weights = c(3.0, 1.0, 1.0),
+                   labels = c(1, 0, 1))
+  columns <- feature_columns(dt, c("weights", "price"))
+  constructed_input_fn <- input_fn(
+    dt,
+    response = "labels",
+    features = c("price", "weights", "example_id"))
 
   ## https://github.com/rstudio/tensorflow/issues/104
-  # clf <-
-  #   svm_classifier(
-  #     feature_columns = columns,
-  #     example_id_column = "id_column",
-  #     weight_column_name = "drat"
-  #   ) %>% fit(input_fn = constructed_input_fn)
+  ## https://github.com/tensorflow/tensorflow/issues/8043
+  clf <-
+    svm_classifier(
+      feature_columns = columns,
+      example_id_column = "example_id",
+      weight_column_name = "weights"
+    )
+  # fit(clf, input_fn = constructed_input_fn)
 })
