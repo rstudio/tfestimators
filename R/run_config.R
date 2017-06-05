5bcd1749f4ede8cddb7d35f6365fa4d985829dd6
terrytangyuan <terrytangyuan@gmail.com>
2017-06-04 22:15:31 -0400

    Add estimator keys

--- R/run_config.R
+++ R/run_config.R
@@ -3,14 +3,13 @@
 #' This class specifies the configurations for an `Estimator` run.
 #' 
 #' @examples
-#' \dontrun{
 #' config <- run_config()
+#' 
 #' # Get the properties of the config
-#' config$keep_checkpoint_every_n_hours
+#' names(config)
 #' 
 #' # Change the mutable properties of the config
-#' config$replace(tf_random_seed = 11, save_summary_steps = 12)
-#' }
+#' config <- config$replace(tf_random_seed = 11L, save_summary_steps = 12L)
 #' 
 #' @family run_config methods
 #'   
@@ -18,7 +17,6 @@
 run_config <- function() {
   estimator_lib$run_config$RunConfig()
 }
-
 
 #' @export
 print.tensorflow.python.estimator.run_config.RunConfig <- function(config) {
