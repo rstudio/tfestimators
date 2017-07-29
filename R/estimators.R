6ef18c6372599908721d3425bc91f46a2879399e
terrytangyuan <terrytangyuan@gmail.com>
2017-07-29 11:06:09 -0400

    Tweak dnn documentation

--- R/estimators.R
+++ R/estimators.R
@@ -17,16 +17,16 @@
 #'   string.
 #'
 #' @param weight_column A string, or a numeric column created by
-#'   [column_numeric()] defining feature column representing weights. It is used
+#'   column_numeric() defining feature column representing weights. It is used
 #'   to down weight or boost examples during training. It will be multiplied by
 #'   the loss of the example. If it is a string, it is used as a key to fetch
 #'   weight tensor from the `features` argument. If it is a numeric column,
-#'   then the raw tensor is fetched by key `weight_column.key`, then
-#'   `weight_column.normalizer_fn` is applied on it to get weight tensor.
+#'   then the raw tensor is fetched by key `weight_column$key`, then
+#'   `weight_column$normalizer_fn` is applied on it to get weight tensor.
 #'   
 #' @param n_classes The number of label classes.
 #'   
-#' @param config A Python `RunConfig` object, used to configure the runtime
+#' @param config A run configuration created by run_config(), used to configure the runtime
 #'   settings.
 #'   
 #' @param input_layer_partitioner An optional partitioner for the input layer.
