eda1798e7b85f623c952360a78364492890122fb
Kevin Ushey <kevinushey@gmail.com>
2017-08-03 14:43:04 -0700

    provide link to API documentation

--- man-roxygen/roxlate-activation-fn.R
+++ man-roxygen/roxlate-activation-fn.R
@@ -1,5 +1,8 @@
 #' @param <%= name %>
 #'   The activation function to apply to each layer. This can either be an
 #'   actual activation function (e.g. \code{tf$nn$relu}), or the name of an
-#'   activation function (e.g. \code{"relu"}). Defaults to the 
-#'   \code{"<%= default %>"} activation function.
+#'   activation function (e.g. \code{"relu"}). Defaults to the
+#'   \code{"<%= default %>"} activation function. See
+#'   \url{https://www.tensorflow.org/versions/r0.12/api_docs/python/nn/activation_functions_}
+#'   for documentation related to the set of activation functions available
+#'   in TensorFlow.
