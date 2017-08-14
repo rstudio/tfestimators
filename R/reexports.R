1b483f8d13d11748e2ae65120f1c8ddf0fe00ef1
jjallaire <jj@rstudio.com>
2017-08-14 09:26:00 -0400

    re-export flags and run_dir from tfruns

--- R/reexports.R
+++ R/reexports.R
@@ -21,3 +21,28 @@ tensorflow::tf_config
 
 #' @export
 tensorflow::tensorboard
+
+#' @importFrom tfruns flags
+#' @export
+tfruns::flags
+
+#' @importFrom tfruns flag_numeric
+#' @export
+tfruns::flag_numeric
+
+#' @importFrom tfruns flag_integer
+#' @export
+tfruns::flag_integer
+
+#' @importFrom tfruns flag_string
+#' @export
+tfruns::flag_string
+
+#' @importFrom tfruns flag_boolean
+#' @export
+tfruns::flag_boolean
+
+#' @importFrom tfruns run_dir
+#' @export
+tfruns::run_dir
+
