3f2c69d6cee9f06e8776d29578f7565f0aa72196
Kevin Ushey <kevinushey@gmail.com>
2017-08-17 15:15:44 -0700

    tweaks to progress bar output

--- R/progress.R
+++ R/progress.R
@@ -5,12 +5,17 @@ hook_progress_bar <- function(steps) {
   else
     ":current/:total [:bar] - ETA: :eta - loss: :loss"
   
-  bar <- progress::progress_bar$new(
+  .values <- NULL
+  .n <- 0
+  .bar <- progress::progress_bar$new(
     format = format,
     total = steps,
     complete = "=",
     incomplete = ".",
-    clear = FALSE
+    clear = FALSE,
+    width = min(getOption("width"), 80),
+    stream = stdout(),
+    show_after = 0
   )
   
   session_run_hook(
@@ -20,9 +25,39 @@ hook_progress_bar <- function(steps) {
     },
     
     after_run = function(context, values) {
+      # update progress bar
       loss <- values$results[[length(values$results)]]
       tokens <- list(loss = format(round(loss, 2), nsmall = 2))
-      bar$tick(tokens = tokens)
+      .bar$tick(tokens = tokens)
+      
+      # save and update state
+      .values <<- values
+      .n <<- .n + 1
+    },
+    
+    end = function(session) {
+      
+      # if we ran as many steps as expected, bail
+      if (.n == steps)
+        return()
+      
+      # otherwise, write a single-tick progress bar encoding the finished state
+      .bar <<- progress::progress_bar$new(
+        format = format,
+        total = .n,
+        complete = "=",
+        incomplete = ".",
+        clear = FALSE,
+        width = min(getOption("width"), 80),
+        stream = stdout(),
+        show_after = 0
+      )
+      
+      # update progress bar
+      values <- .values
+      loss <- values$results[[length(values$results)]]
+      tokens <- list(loss = format(round(loss, 2), nsmall = 2))
+      .bar$tick(len = .n, tokens = tokens)
     }
     
   )
