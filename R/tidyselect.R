19ffda88cdf8e2c47a145358c10f518d0f21954f
Kevin Ushey <kevinushey@gmail.com>
2017-08-28 14:15:31 -0700

    be slightly more succinct

--- R/tidyselect.R
+++ R/tidyselect.R
@@ -1,9 +1,5 @@
 tidyselect_data <- function() {
   tidyselect <- asNamespace("tidyselect")
   exports <- getNamespaceExports(tidyselect)
-  data <- lapply(exports, function(export) {
-    tidyselect[[export]]
-  })
-  names(data) <- exports
-  data
+  mget(exports, tidyselect, inherits = TRUE)
 }
