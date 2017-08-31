5c2e8b4113f86079108cf5695f1bda9783a06476
Kevin Ushey <kevinushey@gmail.com>
2017-08-31 14:27:47 -0700

    remove 'experiment' from API for now

--- R/generic_methods.R
+++ R/generic_methods.R
@@ -52,8 +52,6 @@ export_savedmodel <- function(object, ...) {
 #' 
 #' @param object An \R object.
 #' @param ... Optional arguments passed on to implementing methods.
-#' 
-#' @export
 experiment <- function(object, ...) {
   UseMethod("experiment")
 }
