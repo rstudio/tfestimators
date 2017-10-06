ebfb410ef0d7663f0cd175d7999b7e3abee4a3f2
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-06 15:30:36 -0700

    add save model tests

---
+++ tests/testthat/test-save.R
@@ -0,0 +1,19 @@
+context("Testing model save")
+
+test_that("tf_save() runs successfully", {
+  specs <- mtcars_regression_specs()
+  
+  model <- linear_regressor(feature_columns = specs$linear_feature_columns)
+  model %>% train(input_fn = specs$input_fn, steps = 2)
+  
+  temp_path <- file.path(tempdir(), "testthat-save")
+  if (dir.exists(temp_path)) unlink(temp_path, recursive = TRUE)
+  
+  tf_save(model, path = temp_path)
+  
+  dir_contents <- dir(temp_path, recursive = TRUE)
+  
+  expect_true(any(grepl("saved_model\\.pb", dir_contents)))
+  expect_true(any(grepl("variables\\.data", dir_contents)))
+  expect_true(any(grepl("variables\\.index", dir_contents)))
+})
