ee4830487d7c49d87dda90a197dc1d18ea014ebd
jjallaire <jj@rstudio.com>
2017-10-13 08:53:12 -0400

    import estimator generics from tensorflow package

--- R/generic_methods.R
+++ R/generic_methods.R
@@ -1,38 +1,8 @@
-#' Train a Model
-#' 
-#' Train a model object.
-#' 
-#' @param object A trainable \R object.
-#' @param ... Optional arguments passed on to implementing methods.
-#' 
-#' @export
-train <- function(object, ...) {
-  UseMethod("train")
-}
 
-#' Evaluate a Model
-#' 
-#' Evaluate a model object.
-#' 
-#' @param object An evaluatable \R object.
-#' @param ... Optional arguments passed on to implementing methods.
-#' 
-#' @export
-evaluate <- function(object, ...) {
-  UseMethod("evaluate")
-}
 
-#' Simultaneously Train and Evaluate a Model
-#' 
-#' Train and evaluate a model object.
-#' 
-#' @param object An \R object.
-#' @param ... Optional arguments passed on to implementing methods.
-#' 
-#' @export
-train_and_evaluate <- function(object, ...) {
-  UseMethod("train_and_evaluate")
-}
+#' @importFrom tensorflow evaluate train train_and_evaluate export_savedmodel
+NULL
+
 
 #' Construct an Experiment
 #' 
