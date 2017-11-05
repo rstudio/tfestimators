ca0e93ec6d1bd24634eae348daf65650d70d1b93
terrytangyuan <terrytangyuan@gmail.com>
2017-11-05 16:56:13 -0500

    Fix pkgdown warnings on \dontrun

--- R/session_run_hooks_custom.R
+++ R/session_run_hooks_custom.R
@@ -12,7 +12,7 @@ should_execute <- function(current_step, every_n_step) {
 #' @family session_run_hook wrappers
 #' 
 #' @examples
-#' \dontrun {
+#' \dontrun{
 #' lr <- linear_regressor(
 #'   feature_columns = fcs
 #' ) %>% train(
@@ -65,7 +65,7 @@ hook_history_saver <- function(every_n_step = 2) {
 #' @family session_run_hook wrappers
 #'
 #' @examples
-#' \dontrun {
+#' \dontrun{
 #' lr <- linear_regressor(
 #'   feature_columns = fcs
 #' ) %>% train(
