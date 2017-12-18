ff84a5261d3cf3c4719ba5a078df3310732c0471
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-17 23:04:32 -0800

    add classification export_savedmodel tests

--- tests/testthat/test-save.R
+++ tests/testthat/test-save.R
@@ -53,3 +53,36 @@ test_succeeds("export_savedmodel() runs successfully for dnn_regressor", {
 
   export_test_savedmodel(model)
 })
+
+test_succeeds("export_savedmodel() runs successfully for linear_classifier", {
+  specs <- mtcars_classification_specs()
+  
+  model <- linear_classifier(feature_columns = specs$linear_feature_columns)
+  model %>% train(input_fn = specs$input_fn, steps = 2)
+  
+  export_test_savedmodel(model)
+})
+
+test_succeeds("export_savedmodel() runs successfully for dnn_linear_combined_classifier", {
+  specs <- mtcars_classification_specs()
+  
+  model <- dnn_linear_combined_classifier(
+    linear_feature_columns = specs$linear_feature_columns,
+    dnn_feature_columns = specs$dnn_feature_columns,
+    dnn_hidden_units = c(3, 3))
+  model %>% train(input_fn = specs$input_fn, steps = 2)
+  
+  export_test_savedmodel(model)
+})
+
+test_succeeds("export_savedmodel() runs successfully for dnn_classifier", {
+  specs <- mtcars_classification_specs()
+  
+  model <- dnn_classifier(
+    hidden_units = c(3,3),
+    feature_columns = specs$linear_feature_columns
+  )
+  model %>% train(input_fn = specs$input_fn, steps = 2)
+  
+  export_test_savedmodel(model)
+})
